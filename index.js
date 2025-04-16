6f0e0179b4ec5931dee749bf787a4f3f2be8d31f
AastikP <168633857+AastikP@users.noreply.github.com>
2025-04-16 22:56:56 +0530

    e

--- index.js
+++ index.js
@@ -32,3 +32,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
